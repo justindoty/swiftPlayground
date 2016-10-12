37cdf26300e4412cd60c6a72ac19dc12ef5e6e7e
Justin Doty <justindoty12@gmail.com>
2016-10-11 20:17:10 -0500

    initial commit

---
+++ Contents.swift
@@ -0,0 +1,34 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var namesOfIntegers = [Int: String]()
+
+namesOfIntegers[3] = "three"
+namesOfIntegers[44] = "forty four"
+
+namesOfIntegers = [:]
+
+var airports: [String: String] = ["YYZ": "Toronto Pearson", "LAX": "Los Angeles International"]
+
+print("The airports dictionary has: \(airports.count) items")
+if airports.isEmpty {
+    print("The airports dictionary is empty!")
+}
+
+airports["LHR"] = "London"
+
+airports["LHR"] = "London Heathrow"
+
+airports["DEV"] = "Devslopes International"
+
+airports["DEV"] = nil
+
+for (airportCode, airportName) in airports {
+    print("\(airportCode): \(airportName)")
+    
+}
+
+for key in airports.keys {
+    print("Key: \(key)")
+}
